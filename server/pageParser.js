346483c4c800f4cb8be78ce2864172454ceb8c12
Philipp Piwo <github@piwo.space>
2020-02-26 14:23:25 +0100

    added page parser module

---
+++ server/pageParser.js
@@ -0,0 +1,28 @@
+const SnippetLoader = require('./snippetLoader');
+const DomLoader = require("./domLoader");
+const Aeternity = require("./aeternity");
+
+module.exports = class PageParser {
+
+  constructor() {
+    this.snippetLoader = new SnippetLoader();
+    this.aeternity = new Aeternity();
+  }
+
+  async getAddressesFromPage(url) {
+    const {html} = await DomLoader.getHTMLfromURL(url);
+    if (!html) throw Error("html loading failed");
+
+    const snippets = this.snippetLoader.getSnippetForURL(url);
+
+    return await snippets.reduce(async (promiseAcc, {domRegex}) => {
+      const acc = await promiseAcc;
+      const matches = html.match(new RegExp(domRegex, "g"));
+      const uniqueMatches = [...new Set(matches)];
+
+      const nameResolvedMatches = matches && domRegex.includes('\.chain') ? await this.aeternity.getAddressFromChainName(uniqueMatches) : uniqueMatches;
+
+      return nameResolvedMatches ? acc.concat(nameResolvedMatches) : acc;
+    }, []);
+  }
+};
