ee8bdbcab1cad0e78193476174b44af9b7efdac6
Philipp Piwo <github@piwo.space>
2020-04-30 13:51:46 +0200

    add error context in logs

--- server/logger.js
+++ server/logger.js
@@ -1,16 +1,18 @@
 const winston = require('winston');
 const path = require('path');
 
-const logger = (module) => {
+const logger = (module, contextInfo = null) => {
   const fileName = path.basename(module.id, '.js');
+  const contextMsg = contextInfo ? `${fileName}, ${contextInfo.substr(contextInfo.length - 5)}` : fileName;
+
   return winston.createLogger({
-    level: 'info',
+    level: 'debug',
     format: winston.format.combine(
       winston.format.timestamp(),
       winston.format.colorize(),
       winston.format.printf(msg => {
         const splatMsg = msg[Symbol.for('splat')] ? msg[Symbol.for('splat')].join(" ") : "";
-        return `[${msg.timestamp}] (${fileName}) ${msg.level}: ${msg.message} ${splatMsg} `
+        return `[${msg.timestamp}] (${contextMsg}) ${msg.level}: ${msg.message} ${splatMsg} `
       })
     ),
     transports: [
