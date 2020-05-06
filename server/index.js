f9d3144f5fe8ccee63c6bfdf4e897113012479fa
Philipp Piwo <github@piwo.space>
2020-05-06 10:03:56 +0200

    add error logging for unhandled promise rejections

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
 const OracleService = require('./oracleService');
+const logger = require("./logger")(module);
+
+process.on('unhandledRejection', (reason, p) => {
+  logger.error('Unhandled Rejection at: Promise', p, 'reason:', reason.stack)
+});
 
 const main = async () => {
   const oracleService = new OracleService();
