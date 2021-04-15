72dfdc0d32ec533cd64c0d033e5eb72843497f08
Philipp Piwo <github@piwo.space>
2021-04-15 18:12:39 +0200

    add oracle getter contract

--- .scripts/postinstall.js
+++ .scripts/postinstall.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
 const oracleServiceInterface = fs.readFileSync(__dirname + '/../contracts/OracleServiceInterface.aes', 'utf-8');
-
 fs.writeFileSync(__dirname + '/../OracleServiceInterface.aes.js', `module.exports = \`\n${oracleServiceInterface}\`;\n`, 'utf-8');
+
+const oracleGetter = fs.readFileSync(__dirname + '/../contracts/OracleGetter.aes', 'utf-8');
+fs.writeFileSync(__dirname + '/../OracleGetter.aes.js', `module.exports = \`\n${oracleGetter}\`;\n`, 'utf-8');
